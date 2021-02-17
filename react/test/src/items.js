20de20c9d60e9ce54f4308377b411746ae2552cc
saranakom <saranakom2328@gmail.com>
2021-02-17 15:44:11 +0700

    react

---
+++ react/test/src/items.js
@@ -0,0 +1,24 @@
+const items = [{
+        title: "pic_1",
+        thumbnaiUrl: "/1.jpg"
+
+
+    },
+    {
+        title: "pic_2",
+        thumbnaiUrl: "/2.jpg"
+    },
+    {
+        title: "pic_3",
+        thumbnaiUrl: "/3.jpg"
+
+
+    },
+    {
+        title: "pic_4",
+        thumbnaiUrl: "/4.jpg"
+
+
+    }
+];
+export default items;
